a893ef40967f2af80e5e16968adca10dfcce50cc
Branislav Trstenský <bt7s7k7@hotmail.sk>
2022-10-31 19:53:07 +0100

    Fixed base url for dynamic urls

--- frontend/src/frontend/constants.ts
+++ frontend/src/frontend/constants.ts
@@ -2,6 +2,7 @@ import fallbackIcon from "./fallback-icon.svg?url"
 
 export function getIconURL(icon: string | undefined | null) {
     if (icon) {
+        if (import.meta.env.BASE_URL) return `${import.meta.env.BASE_URL}icons/${icon}`
         return `/icons/${icon}`
     } else {
         return fallbackIcon
@@ -10,6 +11,7 @@ export function getIconURL(icon: string | undefined | null) {
 
 export function getTrackURL(track: string | undefined | null) {
     if (track) {
+        if (import.meta.env.BASE_URL) return `${import.meta.env.BASE_URL}tracks/${track}`
         return `/tracks/${track}`
     } else {
         return undefined
