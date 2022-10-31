850286d0b0a9581a3cf9858ef3a898cf3a9ed0cb
Branislav Trstenský <bt7s7k7@hotmail.sk>
2022-10-31 19:15:12 +0100

    Using .env.local on frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,13 @@
 import vue from "@vitejs/plugin-vue"
 import vueJsx from "@vitejs/plugin-vue-jsx"
 import * as dotenv from "dotenv"
+import path from "path"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
     dotenv.config()
+    dotenv.config({ path: path.resolve(__dirname, ".env.local") })
 
     return {
         plugins: [vue(), vueJsx()],
